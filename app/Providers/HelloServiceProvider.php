90c0c11220ee939e9d5da7179a5e5eb65a712df8
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-09-06 16:04:50 +0900

    indexまで表示

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -3,14 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
+use Validator;
+use App\Http\Validators\HelloValidator;
 use Illuminate\Support\ServiceProvider;
+
+
 
 class HelloServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        View::composer(
-            'hello.index','App\Http\Composers\HelloComposer'
-        );
+        $validator = $this->app['validator'];
+        $validator->resolver(function($translator,$data,$rules,$messages){
+            return new HelloValidator($translator,$data,$rules,$messages);
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\HelloMiddleware;
 
-Route::get('hello','HelloController@index')
-->middleware('helo');
+Route::get('hello','HelloController@index');
+Route::post('hello','HelloController@post');
+Route::get('hello/add','HelloController@add');
+Route::post('hello/add','HelloController@create');
+Route::get('hello/edit','HelloController@edit');
+Route::post('hello/edit','HelloController@update');
+Route::get('hello/del','HelloController@del');
+Route::post('hello/del','HelloController@remove');
