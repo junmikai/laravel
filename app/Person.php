8a2f436af1ed367b052237b6fd8fb64f09ba8ec7
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-09-08 10:20:05 +0900

    モデルまで

--- app/Person.php
+++ app/Person.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
+
+    public function getData()
+    {
+        return $this->id . ':' . $this->name . '(' . $this->age . ')';
+    }
 }
